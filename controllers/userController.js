71c430d861aba2d3becc70b0943ffbf74b1f2ca0
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-28 00:59:13 +0530

    verify otp routes and control completed all functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel')
+const Otp = require('../models/userOtpModel')
 const securePassword = require('../utils/securePassword')
 const cloudinary = require('../utils/cloudinary')
 const sendEmail = require('../utils/nodeMailer')
@@ -33,4 +34,32 @@ const userRegistration = async (req,res) =>{
     }
 }
 
-module.exports = {userRegistration}
+const otpVerify = async (req,res) =>{
+    try {
+        const {enteredValues,userId} = req.body
+        const otpData = await Otp.find({userId:userId})
+        const {expiresAt}= otpData[otpData.length-1]
+        const correctOtp = otpData[otpData.length-1].otp
+        if(otpData&&expiresAt<Date.now()){
+            return res.status(401).json({message:'Email otp has expired'})
+        }
+        if(correctOtp==enteredValues){
+            await Otp.deleteMany({userId:userId});
+            await User.updateOne(
+                {_id:userId},
+                {$set:{otp_verified:true}}
+            );
+            res.status(200).json({
+                status:true,
+                message: 'User registered successfully , you can login now',
+            })
+        }else{
+            res.status(400).json({ status: false, message: "Incorrect OTP" });
+        }
+
+    } catch (error) {
+        res.status(400).json({status:false,message:'incorrect otp'})
+    }
+}
+
+module.exports = {userRegistration,otpVerify}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController.js")
 
 
 router.post("/userSignup", userController.userRegistration);
+router.post("/otpVerify",userController.otpVerify)
 
 module.exports = router
